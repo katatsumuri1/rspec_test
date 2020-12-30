c6dec48d71d579621f9c4a94d276e6f08322edff
EC2 Default User <ec2-user@ip-172-31-30-47.ap-northeast-1.compute.internal>
2020-12-30 06:21:18 +0000

    finish rspec_4章

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module Projects
     config.generators do |g|
       
       g.test_framework  :rspec,
-        fixtures:       false,
         view_specs:     false,
         helper_spec:    false,
         routing_specs:  false
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
   
-  # ユーザー単位では重複したプロジェクト名を許可しないこと
+  #たくさんのメモがついていること
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
+  end
   
+  # ユーザー単位では重複したプロジェクト名を許可しないこと
   it "does not allow duplicate project names per user"do
     
-    user = User.create(
-      first_name: "joe",
-      last_name:  "Tester",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
-      )
+    user = FactoryBot.create(:user)
     
     user.projects.create(
       name:"Test Project",
@@ -28,29 +28,36 @@ RSpec.describe Project, type: :model do
   
   #二人のユーザーが同じ名前を使う事は許可すること
   it "allows two users to sheare a project name" do
-    user = User.create(
-      first_name: "Joe",
-      last_name:  "Tester",
-      email:      "joetester@example.com",
-      password:   "dottle-nouveau-pavikion-tights-furuze",
-      )
-      
+    user = FactoryBot.create(:user)
     user.projects.create(
       name: "Test Project",
       )
       
-    other_user = User.create(
-      first_name: "Jane",
-      last_name:  "Tester",
-      email:      "janetester@example.com",
-      password:   "dottle-nouveau-pavikion-tights-furze",
-      )
-      
+    other_user = FactoryBot.create(:user)
     other_project= other_user.projects.build(
       name: "Test Project",
       )
-        
-      expect(other_project).to be_valid
+      
+    expect(other_project).to be_valid
       
     end
+  #遅延ステータス
+  describe "late status" do
+    #締め切り日がすぎていれば遅延していること
+    it "is late when the due date is past today" do
+      project = FactoryBot.create(:project, :due_yesterday)
+      expect(project).to be_late
+    end
+  end
+  #締め切り日が今日ならスケジュール通りであること
+  it "is on time when the due date is today" do
+    project = FactoryBot.create(:project, :due_today)
+    expect(project).to_not be_late
+  end
+  
+  # 締め切り日が未来ならスケジュール通りであること
+  it "is on time when the due date is in the future" do
+    project = FactoryBot.create(:project, :due_tomorrow)
+    expect(project).to_not be_late
+  end
 end
